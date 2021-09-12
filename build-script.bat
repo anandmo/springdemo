099e3e92a3158cc1563dc14ac79fa69ba0be9c62
Anand Mohan <im.anand.m@gmail.com>
2021-09-12 03:09:27 -0700

    Modified build script

--- build-script.bat
+++ build-script.bat
@@ -1,6 +1,6 @@
 echo "Cleaning and building the spring boot application"
 call mvn clean package
 echo "Copying the build executable to AWS server"
-pscp -i C:\Software Development Projects\springdemo\keys\springdemog.ppk .\target\springdemo-0.0.1-SNAPSHOT.jar ec2-user@ec2-18-224-184-198.us-east-2.compute.amazonaws.com:/home/ec2-user
+pscp -i "C:\Software Development Projects\springdemo\keys\springdemog.ppk .\target\springdemo-0.0.1-SNAPSHOT.jar" ec2-user@ec2-18-224-184-198.us-east-2.compute.amazonaws.com:/home/ec2-user
 echo "Starting spring boot application on AWS server"
-putty.exe -i C:\Software Development Projects\springdemo\keys\springdemog.ppk -m .\deploy.sh ec2-user@ec2-18-224-184-198.us-east-2.compute.amazonaws.com
+putty.exe -i "C:\Software Development Projects\springdemo\keys\springdemog.ppk" -m .\deploy.sh ec2-user@ec2-18-224-184-198.us-east-2.compute.amazonaws.com
